7ef300e9f43fc14b16bfdfe1dc0eb4ebbdd4b8d5
cpo <none@example.com>
2009-03-25 01:03:30 +0000

    Add xtcreader to new app directory.

---
+++ app/constituents.mk
@@ -0,0 +1,5 @@
+tgtnames = xtcreader
+
+tgtsrcs_xtcreader := xtcreader.cc
+tgtlibs_xtcreader := pdsdata/xtcdata pdsdata/acqdata
+tgtslib_xtcreader := /usr/lib/rt
--- packages.mk
+++ packages.mk
@@ -1,2 +1,2 @@
 # List of packages (low level first)
-packages := acqiris camera evr opal1k xtc
+packages := acqiris camera evr opal1k xtc app
