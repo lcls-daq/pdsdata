20a6802b20a9739c1426ac43908f15db0884467a
weaver <none@example.com>
2009-06-01 17:35:26 +0000

    Tag V00-04-00

--- xtc/constituents.mk
+++ xtc/constituents.mk
@@ -7,6 +7,7 @@ libsrcs_xtcdata := src/TypeId.cc \
                    src/ClockTime.cc \
                    src/TimeStamp.cc \
                    src/Sequence.cc \
+                   src/ClockTime.cc \
                    src/Level.cc \
                    src/ProcInfo.cc \
                    src/DetInfo.cc
