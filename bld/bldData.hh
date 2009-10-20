78475ef5a5fd185c96476914e84a4ade31179b35
tomytsai <none@example.com>
2009-10-20 22:20:36 +0000

    Add units in PhaseCavity bld class comments

--- bld/bldData.hh
+++ bld/bldData.hh
@@ -10,7 +10,6 @@ class BldDataFEEGasDetEnergy
 {
     // PV names: GDET:FEE1:11:ENRC,GDET:FEE1:12:ENRC,GDET:FEE1:21:ENRC,GDET:FEE1:22:ENRC
 public:
-    /* Data structure */
     double f_11_ENRC;   /* in mJ */ 
     double f_12_ENRC;   /* in mJ */ 
     double f_21_ENRC;   /* in mJ */
@@ -39,11 +38,10 @@ class BldDataPhaseCavity
     // PV names: UND:R02:IOC:16:BAT:FitTime1, UND:R02:IOC:16:BAT:FitTime2, 
     //           UND:R02:IOC:16:BAT:Charge1,  UND:R02:IOC:16:BAT:Charge2
 public:
-    /* Data structure */
-    double fFitTime1;   /* in ?? Unit */ 
-    double fFitTime2;   /* in ?? Unit */ 
-    double fCharge1;    /* in ?? Unit */ 
-    double fCharge2;    /* in ?? Unit */ 
+    double fFitTime1;   /* in pico-seconds */ 
+    double fFitTime2;   /* in pico-seconds */ 
+    double fCharge1;    /* in pico-columbs */ 
+    double fCharge2;    /* in pico-columbs */ 
     
     int print() const;
 };
--- bld/src/bldData.cc
+++ bld/src/bldData.cc
@@ -5,10 +5,10 @@ using namespace Pds;
 
 int BldDataFEEGasDetEnergy::print() const
 {    
-    printf("GDET:FEE:11:ENRC: %lf\n", f_11_ENRC );
-    printf("GDET:FEE:12:ENRC: %lf\n", f_12_ENRC );
-    printf("GDET:FEE:21:ENRC: %lf\n", f_21_ENRC );
-    printf("GDET:FEE:22:ENRC: %lf\n", f_22_ENRC );
+    printf("GDET:FEE:11:ENRC ( in mJ ): %lf\n", f_11_ENRC );
+    printf("GDET:FEE:12:ENRC ( in mJ ): %lf\n", f_12_ENRC );
+    printf("GDET:FEE:21:ENRC ( in mJ ): %lf\n", f_21_ENRC );
+    printf("GDET:FEE:22:ENRC ( in mJ ): %lf\n", f_22_ENRC );
     
     return 0;
 }
@@ -26,10 +26,10 @@ int BldDataEBeam::print() const
 
 int BldDataPhaseCavity::print() const
 {    
-    printf("FitTime1: %lf\n", fFitTime1 );
-    printf("FitTime2: %lf\n", fFitTime2 );
-    printf("Charge1:  %lf\n", fCharge1 );
-    printf("Charge2:  %lf\n", fCharge2 );
+    printf("FitTime1 ( in pico-seconds ): %lf\n", fFitTime1 );
+    printf("FitTime2 ( in pico-seconds ): %lf\n", fFitTime2 );
+    printf("Charge1  ( in pico-columbs ): %lf\n", fCharge1 );
+    printf("Charge2  ( in pico-columbs ): %lf\n", fCharge2 );
     
     return 0;
 }
