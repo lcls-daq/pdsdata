a7c50768d66f5bd133690a1ce3839546a03b3022
tomytsai <none@example.com>
2010-03-01 22:03:50 +0000

    *** empty log message ***

--- princeton/ConfigV1.hh
+++ princeton/ConfigV1.hh
@@ -14,13 +14,14 @@ namespace Princeton
 class ConfigV1 
 {
 public:
-  static const int Version                        = 1;
-  static const unsigned int uPI_MTE_1300_Width    = 1340;
-  static const unsigned int uPI_MTE_1300_Height   = 1300;
+  static const int Version                              = 1;
+  static const unsigned int uPI_MTE_1300_Width          = 1340;
+  static const unsigned int uPI_MTE_1300_Height         = 1300;
   //static const unsigned int uPI_MTE_2048_Width    = 2048;
   //static const unsigned int uPI_MTE_2048_Height   = 2048;
-  static const unsigned int uPI_MTE_2048_Width    = 512; // !! for debug only
-  static const unsigned int uPI_MTE_2048_Height   = 512;
+  static const unsigned int uPI_MTE_2048_Width          = 512; // !! for debug only
+  static const unsigned int uPI_MTE_2048_Height         = 512;
+  static const int          iPI_MTE_2048_MaxSpeedIndex  = 5;
   
   enum EnumExposureMode // copied from pvcam.h
   { 
@@ -33,21 +34,33 @@ public:
     INT_STROBE_MODE
   };  
   
-  ConfigV1(uint32_t uWidth = uPI_MTE_2048_Width, uint32_t uHeight = uPI_MTE_2048_Height, 
-   uint32_t uOrgX = 0, uint32_t uOrgY = 0, uint32_t uBinX = 1, uint32_t uBinY = 1,
-   int16_t  iCoolingTemp = -1000, EnumExposureMode iExposureMode = STROBED_MODE, 
-   float f32ExposureTime = 0.005, uint8_t uMakeUpEventMode = 0 );
+  ConfigV1(
+   uint32_t         uWidth                = uPI_MTE_2048_Width, 
+   uint32_t         uHeight               = uPI_MTE_2048_Height, 
+   uint32_t         uOrgX                 = 0, 
+   uint32_t         uOrgY                 = 0, 
+   uint32_t         uBinX                 = 1, 
+   uint32_t         uBinY                 = 1,
+   int16_t          iCoolingTemp          = -1000, 
+   EnumExposureMode iExposureMode         = STROBED_MODE, 
+   float            f32ExposureTime       = 0.005, 
+   int16_t          i16ReadoutSpeedIndex  = iPI_MTE_2048_MaxSpeedIndex,
+   uint8_t          uDelayMode            = 0 );
 
-  uint32_t          width()           { return _uWidth; }
-  uint32_t          height()          { return _uHeight; }
-  uint32_t          orgX()            { return _uOrgX; }
-  uint32_t          orgY()            { return _uOrgY; }    
-  uint32_t          binX()            { return _uBinX; }
-  uint32_t          binY()            { return _uBinY; }    
-  int16_t           coolingTemp()     { return _i16CoolingTemp; }
-  EnumExposureMode  exposureMode()    { return (EnumExposureMode) _i16ExposureMode; }
-  float             exposureTime()    { return _f32ExposureTime; }
-  uint8_t           makeUpEventMode() { return _u8MakeUpEventMode; }
+  uint32_t          width()             { return _uWidth; }
+  uint32_t          height()            { return _uHeight; }
+  uint32_t          orgX()              { return _uOrgX; }
+  uint32_t          orgY()              { return _uOrgY; }    
+  uint32_t          binX()              { return _uBinX; }
+  uint32_t          binY()              { return _uBinY; }    
+  int16_t           coolingTemp()       { return _i16CoolingTemp; }
+  EnumExposureMode  exposureMode()      { return (EnumExposureMode) _i16ExposureMode; }
+  float             exposureTime()      { return _f32ExposureTime; }
+  int16_t           readoutSpeedIndex() { return _i16ReadoutSpeedIndex; }
+  uint8_t           makeUpEventMode()   { return _u8DelayMode; }
+
+  uint32_t          setWidth(uint32_t uWidth)     { return _uWidth = uWidth; }
+  uint32_t          setHeight(uint32_t uHeight)   { return _uHeight = uHeight; }
   
   int               frameSize();  // calculate the frame size based on the current ROI and binning settings
   
@@ -58,7 +71,8 @@ private:
   int16_t           _i16CoolingTemp;
   int16_t           _i16ExposureMode;
   float             _f32ExposureTime;
-  uint8_t           _u8MakeUpEventMode;
+  int16_t           _i16ReadoutSpeedIndex;
+  uint8_t           _u8DelayMode;
 };
 
 } // namespace Princeton
--- princeton/src/ConfigV1.cc
+++ princeton/src/ConfigV1.cc
@@ -6,11 +6,21 @@
 using namespace Pds;
 using namespace Princeton;
 
-ConfigV1::ConfigV1(uint32_t uWidth, uint32_t uHeight, uint32_t uOrgX, uint32_t uOrgY, uint32_t uBinX, uint32_t uBinY,
-   int16_t i16CoolingTemp, EnumExposureMode enumExposureMode, float f32ExposureTime, uint8_t u8MakeUpEventMode) :
+ConfigV1::ConfigV1(
+ uint32_t         uWidth, 
+ uint32_t         uHeight, 
+ uint32_t         uOrgX, 
+ uint32_t         uOrgY, 
+ uint32_t         uBinX, 
+ uint32_t         uBinY,
+ int16_t          i16CoolingTemp, 
+ EnumExposureMode enumExposureMode, 
+ float            f32ExposureTime, 
+ int16_t          i16ReadoutSpeedIndex,
+ uint8_t          u8DelayMode) :
  _uWidth(uWidth), _uHeight(uHeight), _uOrgX(uOrgX), _uOrgY(uOrgY), _uBinX(uBinX), _uBinY(uBinY),
  _i16CoolingTemp(i16CoolingTemp), _i16ExposureMode((int16_t)enumExposureMode), _f32ExposureTime(f32ExposureTime),
- _u8MakeUpEventMode(u8MakeUpEventMode)
+ _i16ReadoutSpeedIndex(i16ReadoutSpeedIndex), _u8DelayMode(u8DelayMode)
  {}
 
 int ConfigV1::frameSize()
