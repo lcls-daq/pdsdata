cfcc3944e0d4c6e1406a50dd01ad304f59d5f30d
caf <none@example.com>
2010-05-10 21:28:49 +0000

    added new methods for reading voltages of channels 0-3 as floats.

--- ipimb/DataV1.hh
+++ ipimb/DataV1.hh
@@ -6,6 +6,11 @@
 #include "pdsdata/xtc/TypeId.hh"
 #include "ConfigV1.hh"
 
+// macros for converting IPIMB counts to voltages
+#define IPIMB_ADC_RANGE 3.3
+#define IPIMB_ADC_STEPS 65536
+#define IPIMB_COUNTS_TO_VOLTS(x) ((float) (x) * IPIMB_ADC_RANGE) / (IPIMB_ADC_STEPS - 1);
+
 namespace Pds {
 
   namespace Ipimb {
@@ -21,6 +26,10 @@ namespace Pds {
       uint16_t channel1() const;
       uint16_t channel2() const;
       uint16_t channel3() const;
+      float channel0Volts() const;
+      float channel1Volts() const;
+      float channel2Volts() const;
+      float channel3Volts() const;
       uint16_t checksum() const;
 
       static Pds::TypeId typeId();
