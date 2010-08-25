8a24b643cd66dfb76b6559503243f8d867a17615
jackp <none@example.com>
2010-08-25 02:42:42 +0000

    Tue Aug 24 19:41:49 PDT 2010 jackp Changes to the configuration of CsPad and getting from the database

--- cspad/ConfigV1.hh
+++ cspad/ConfigV1.hh
@@ -12,7 +12,7 @@ namespace Pds
   {
     enum {MaxQuadsPerSensor=4, ASICsPerQuad=16};
     enum {RowsPerBank=26, FullBanksPerASIC=7, BanksPerASIC=8, ColumnsPerASIC=185, MaxRowsPerASIC=194};
-    enum {PotsPerQuad=80};
+    enum {PotsPerQuad=80, TwoByTwosPerQuad=4};
     enum RunModes  {NoRunning, RunButDrop, RunAndSendToRCE, RunAndSendTriggeredByTTL, ExternalTriggerSendToRCE, ExternalTriggerDrop, NumberOfRunModes };
     enum DataModes {normal=0, shiftTest=1, testData=2, reserved=3};
 
@@ -53,21 +53,20 @@ namespace Pds
         class CsPadGainMapCfg;
 
       public:
+        ConfigV1QuadReg() {};
         ConfigV1QuadReg(
-            uint32_t         shiftSelect       = 0,
-            uint32_t         edgeSelect        = 0,
-            uint32_t         readClkSet        = 0,
-            uint32_t         readClkHold       = 0,
-            uint32_t         dataMode          = 0,
-            uint32_t         prstSel           = 0,
-            uint32_t         acqDelay          = 0,
-            uint32_t         intTime           = 0,
-            uint32_t         digDelay          = 0,
-            uint32_t         ampIdle           = 0,
-            uint32_t         injTotal          = 0,
-            uint32_t         rowColShiftPer    = 0) :
-            _shiftSelect(shiftSelect),
-            _edgeSelect(edgeSelect),
+            uint32_t         shiftSelect,
+            uint32_t         edgeSelect,
+            uint32_t         readClkSet,
+            uint32_t         readClkHold,
+            uint32_t         dataMode,
+            uint32_t         prstSel,
+            uint32_t         acqDelay,
+            uint32_t         intTime,
+            uint32_t         digDelay,
+            uint32_t         ampIdle,
+            uint32_t         injTotal,
+            uint32_t         rowColShiftPer) :
             _readClkSet(readClkSet),
             _readClkHold(readClkHold),
             _dataMode(dataMode),
@@ -77,10 +76,13 @@ namespace Pds
             _digDelay(digDelay),
             _ampIdle(ampIdle),
             _injTotal(injTotal),
-            _rowColShiftPer(rowColShiftPer) {};
+            _rowColShiftPer(rowColShiftPer) {
+          _shiftSelect[0] = _shiftSelect[1] = _shiftSelect[2] = _shiftSelect[3] = shiftSelect;
+          _edgeSelect[0] = _edgeSelect[1] = _edgeSelect[3] = _edgeSelect[3] = edgeSelect;
+        };
 
-        uint32_t           shiftSelect()        const   { return _shiftSelect;    }
-        uint32_t           edgeSelect()         const   { return _edgeSelect;     }
+        uint32_t           shiftSelect()        const   { return _shiftSelect[0]; }
+        uint32_t           edgeSelect()         const   { return _edgeSelect[0];  }
         uint32_t           readClkSet()         const   { return _readClkSet;     }
         uint32_t           readClkHold()        const   { return _readClkHold;    }
         uint32_t           dataMode()           const   { return _dataMode;       }
@@ -95,14 +97,14 @@ namespace Pds
         const Pds::CsPad::CsPadReadOnlyCfg&     ro      ()  const { return _readOnly;       }
         Pds::CsPad::CsPadDigitalPotsCfg&        dp      ()        { return _digitalPots;    }
         const Pds::CsPad::CsPadDigitalPotsCfg&  dp      ()  const { return _digitalPots;    }
-        Pds::CsPad::CsPadGainMapCfg*            gm      ()        { return &_gainMap;        }
-        const Pds::CsPad::CsPadGainMapCfg*      gm      ()  const { return &_gainMap;        }
+        Pds::CsPad::CsPadGainMapCfg*            gm      ()        { return &_gainMap;       }
+        const Pds::CsPad::CsPadGainMapCfg*      gm      ()  const { return &_gainMap;       }
         Pds::CsPad::CsPadReadOnlyCfg*           readOnly()        { return &_readOnly;      }
         const Pds::CsPad::CsPadReadOnlyCfg*     readOnly()  const { return &_readOnly;      }
 
       private:
-        uint32_t                     _shiftSelect;
-        uint32_t                     _edgeSelect;
+        uint32_t                     _shiftSelect[TwoByTwosPerQuad];
+        uint32_t                     _edgeSelect[TwoByTwosPerQuad];
         uint32_t                     _readClkSet;
         uint32_t                     _readClkHold;
         uint32_t                     _dataMode;
@@ -123,8 +125,29 @@ namespace Pds
     {
       public:
         static const int Version               = 1;
-        ConfigV1() : _testDataIndex(0), _quadMask(0xf) {};
-        ConfigV1(uint32_t t, uint32_t m) : _testDataIndex(t), _quadMask(m) {};
+        ConfigV1() {};
+        ConfigV1(
+            uint32_t runDelay,
+            uint32_t eventCode,
+            uint32_t inactiveRunMode,
+            uint32_t activeRunMode,
+            uint32_t testDataIndex,
+            uint32_t payloadPerQuad,
+            uint32_t badAsicMask0,
+            uint32_t badAsicMask1,
+            uint32_t AsicMask,
+            uint32_t quadMask) :
+          _runDelay(runDelay),
+          _eventCode(eventCode),
+          _inactiveRunMode(inactiveRunMode),
+          _activeRunMode(activeRunMode),
+          _testDataIndex(),
+          _payloadPerQuad(payloadPerQuad),
+          _badAsicMask0(badAsicMask0),
+          _badAsicMask1(badAsicMask1),
+          _AsicMask(AsicMask),
+          _quadMask(quadMask) {};
+
         ConfigV1QuadReg*       quads        ()       { return _quads; }
         const ConfigV1QuadReg* quads        () const { return _quads; }
         uint32_t               tdi          () const { return _testDataIndex; }
@@ -134,7 +157,8 @@ namespace Pds
         uint32_t               inactiveRunMode()const{ return _inactiveRunMode; }
         uint32_t               activeRunMode() const { return _activeRunMode; }
         uint32_t               payloadSize  () const { return _payloadPerQuad; }
-        uint32_t               badAsicMask  () const { return _badAsicMask; }
+        uint32_t               badAsicMask0 () const { return _badAsicMask0; }
+        uint32_t               badAsicMask1 () const { return _badAsicMask1; }
         uint32_t               asicMask     () const { return _AsicMask; }
         uint32_t               numAsicsRead () const { return (_AsicMask&0xf)==1 ? 4 : 16; }
         static const int       version      ()       { return Version; }
@@ -145,8 +169,9 @@ namespace Pds
         uint32_t          _activeRunMode;
         uint32_t          _testDataIndex;
         uint32_t          _payloadPerQuad;
-        uint32_t          _badAsicMask;
-        uint32_t          _AsicMask;
+        uint32_t          _badAsicMask0;
+        uint32_t          _badAsicMask1;
+        uint32_t          _AsicMask;  // there are a maximum of 64 ASICs
         uint32_t          _quadMask;
         ConfigV1QuadReg   _quads[MaxQuadsPerSensor];
     };
