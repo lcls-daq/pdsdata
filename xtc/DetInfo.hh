94ef553fe3b788356bb8209ca929769fdb84950b
tomytsai <none@example.com>
2010-08-31 23:35:54 +0000

    *** empty log message ***

--- xtc/DetInfo.hh
+++ xtc/DetInfo.hh
@@ -4,44 +4,51 @@
 #include <stdint.h>
 #include "pdsdata/xtc/Src.hh"
 
-namespace Pds {
+namespace Pds
+{
 
   class Node;
 
-  class DetInfo : public Src {
+  class DetInfo:public Src
+  {
   public:
     /*
      * Notice: New enum values should be appended to the end of the enum list, since
      *   the old values have already been recorded in the existing xtc files. 
      */
-    enum Detector {NoDetector,
-           AmoIms,AmoGasdet,AmoETof,AmoITof,AmoMbes,AmoVmi,AmoBps,Camp,
-           EpicsArch,BldEb,
-		   SxrBeamline,SxrEndstation,
-		   XppSb1Ipm,XppSb1Pim,XppMonPim,XppSb2Ipm,XppSb3Ipm,XppSb3Pim,XppSb4Pim,XppGon,XppLas,XppEndstation,
-           AmoEndstation,
-           CxiEndstation,
-           XcsEndstation,
-           MecEndstation,
-		   NumDetector};
-    enum Device   {NoDevice,Evr,Acqiris,Opal1000,TM6740,pnCCD,Princeton,Fccd,Ipimb,Encoder,Cspad,NumDevice};
+    enum Detector
+    { NoDetector,
+      AmoIms, AmoGasdet, AmoETof, AmoITof, AmoMbes, AmoVmi, AmoBps, Camp,
+      EpicsArch, BldEb,
+      SxrBeamline, SxrEndstation,
+      XppSb1Ipm, XppSb1Pim, XppMonPim, XppSb2Ipm, XppSb3Ipm, XppSb3Pim,
+        XppSb4Pim, XppGon, XppLas, XppEndstation,
+      AmoEndstation,
+      CxiEndstation,
+      XcsEndstation,
+      MecEndstation,
+      NumDetector
+    };
+    enum Device
+    { NoDevice, Evr, Acqiris, Opal1000, TM6740, pnCCD, Princeton, Fccd, Ipimb,
+      Encoder, Cspad, NumDevice };
 
-    DetInfo() {}
-    DetInfo(uint32_t processId,
-            Detector det, uint32_t detId,
-            Device dev,   uint32_t devId);
+      DetInfo()
+    {
+    }
+    DetInfo(uint32_t processId, Detector det, uint32_t detId, Device dev, uint32_t devId);
 
-    bool operator==(const DetInfo&) const;
+    bool operator==(const DetInfo &) const;
 
     uint32_t processId() const;
-    Detector detector()  const;
-    Device   device()    const;
-    uint32_t detId()     const;
-    uint32_t devId()     const;
+    Detector detector() const;
+    Device device() const;
+    uint32_t detId() const;
+    uint32_t devId() const;
 
-    static const char* name(Detector);
-    static const char* name(Device);
-    static const char* name(const DetInfo&);
+    static const char *name(Detector);
+    static const char *name(Device);
+    static const char *name(const DetInfo &);
   };
 
 }
