67382e493bc3f53165e5cdc7fce2d44956289a4a
jackp <none@example.com>
2012-01-06 03:53:26 +0000

    Thu Jan  5 19:10:38 PST 2012 jackp Added phasics

--- packages.mk
+++ packages.mk
@@ -1,7 +1,13 @@
 # List of packages (low level first)
 ifneq ($(findstring ppc-rtems-rce,$(tgt_arch)),)
 packages := xtc
-else
+endif
+
+ifneq ($(findstring x86_64-linux,$(tgt_arch)),)
+packages := xtc camera phasics
+endif
+
+ifneq ($(findstring i386-linux,$(tgt_arch)),)
 packages := ipimb encoder pnCCD acqiris camera evr opal1k pulnix control xtc \
             epics bld princeton fccd cspad lusi app xamps ana index fexamp gsc16ai \
             phasics timepix
