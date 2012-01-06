37df27c5e36be74b2510c45d3c9ddffd9e463cc3
weaver <none@example.com>
2012-01-06 23:53:39 +0000

    *** empty log message ***

--- packages.mk
+++ packages.mk
@@ -1,13 +1,7 @@
 # List of packages (low level first)
 ifneq ($(findstring ppc-rtems-rce,$(tgt_arch)),)
 packages := xtc
-endif
-
-ifneq ($(findstring x86_64-linux,$(tgt_arch)),)
-packages := xtc camera phasics timepix
-endif
-
-ifneq ($(findstring i386-linux,$(tgt_arch)),)
+else
 packages := ipimb encoder pnCCD acqiris camera evr opal1k pulnix control xtc \
             epics bld princeton fccd cspad lusi app xamps ana index fexamp gsc16ai \
             phasics timepix
