78055b39a8b2b3019a0d257a9a98e2126b548aef
tomytsai <none@example.com>
2012-01-24 00:43:55 +0000

    *** empty log message ***

--- packages.mk
+++ packages.mk
@@ -3,6 +3,6 @@ ifneq ($(findstring ppc-rtems-rce,$(tgt_arch)),)
 packages := xtc
 else
 packages := ipimb encoder pnCCD acqiris camera evr opal1k pulnix control xtc \
-            epics bld princeton fccd cspad lusi app xamps ana index fexamp gsc16ai \
+            epics bld princeton fccd cspad lusi ana index app xamps fexamp gsc16ai \
             phasics timepix cspad2x2
 endif
