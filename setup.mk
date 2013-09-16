4852061aeae2063d9719aac6577fb412f3a673a9
weaver <none@example.com>
2013-09-16 20:07:46 +0000

    Added opt-dbg dbg-opt targets

--- setup.mk
+++ setup.mk
@@ -1,4 +1,8 @@
-archs := i386-linux-opt i386-linux-dbg x86_64-linux-opt x86_64-linux-dbg special
+arch_size = i386-linux x86_64-linux
+arch_opts = -opt -dbg -opt-dbg -dbg-opt
+
+archs := $(foreach aopt,$(arch_opts),$(addsuffix $(aopt),$(arch_size)))
+
 quiet := @
 
 setup.mk:;
