4d9c1d1e109bf0151fd550d9b87c5232a393de24
davidsch <none@example.com>
2014-10-10 20:10:27 +0000

    pdsdata: update to generate code for ddl types with the [[devel]] tag. Do make -C pdsdata GENDEVEL=1 gen rather than make -C pdsdata gen to generate code for 'in development' types

--- psddl/package.mk
+++ psddl/package.mk
@@ -6,6 +6,10 @@
 # Check release location variables
 export RELEASE_DIR := $(PWD)/..
 
+ifdef GENDEVEL
+export PARSEDEVEL := -D
+endif
+
 ddls     := $(filter-out xtc.ddl, $(notdir $(wildcard $(RELEASE_DIR)/psddldata/data/*.ddl)))
 ddl_src  := $(patsubst %.ddl,src/%.ddl.cpp,$(ddls))
 
@@ -23,6 +27,6 @@ gen: pre-gen $(ddl_src)
 #  Machine generate the code
 #
 src/%.ddl.cpp:
-	$(quiet)cd $(RELEASE_DIR) && psddlc -b pdsdata -I data -E pdsdata/psddl -O pdsdata/psddl/src -i pdsdata/psddl -t Pds psddldata/data/$*.ddl && cd $(OLDPWD)
+	$(quiet)cd $(RELEASE_DIR) && psddlc -b pdsdata -I data -E pdsdata/psddl $(PARSEDEVEL) -O pdsdata/psddl/src -i pdsdata/psddl -t Pds psddldata/data/$*.ddl && cd $(OLDPWD)
 
 package.mk:;
