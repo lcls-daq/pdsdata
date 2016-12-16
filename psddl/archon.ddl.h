1dfd2ec45ec06382bf4eb69ae75f5ee38c523510
ddamiani <none@example.com>
2016-12-16 18:18:20 +0000

    improved archon ddl

--- psddl/archon.ddl.h
+++ psddl/archon.ddl.h
@@ -23,7 +23,12 @@ public:
   enum { TypeId = Pds::TypeId::Id_ArchonConfig /**< XTC type ID value (from Pds::TypeId class) */ };
   enum { Version = 1 /**< XTC type version number */ };
   enum { FILENAME_CHAR_MAX = 256 };
-  ConfigV1(const char* arg__config);
+  enum ReadoutMode {
+    Single = 0,
+    Continuous = 1,
+    Triggered = 2,
+  };
+  ConfigV1(Archon::ConfigV1::ReadoutMode arg__readoutMode, uint16_t arg__sweepCount, uint32_t arg__integrationTime, uint32_t arg__nonIntegrationTime, uint32_t arg__preSkipPixels, uint32_t arg__pixels, uint32_t arg__postSkipPixels, uint32_t arg__overscanPixels, uint16_t arg__preSkipLines, uint16_t arg__lines, uint16_t arg__postSkipLines, uint16_t arg__overScanLines, uint16_t arg__horizontalBinning, uint16_t arg__verticalBinning, uint16_t arg__rgh, uint16_t arg__rgl, uint16_t arg__shp, uint16_t arg__shd, uint16_t arg__st, uint16_t arg__stm1, uint16_t arg__at, uint16_t arg__dwell1, uint16_t arg__dwell2, int16_t arg__rgHigh, int16_t arg__rgLow, int16_t arg__sHigh, int16_t arg__sLow, int16_t arg__aHigh, int16_t arg__aLow, int16_t arg__rgSlew, int16_t arg__sSlew, int16_t arg__aSlew, const char* arg__config);
   ConfigV1() {}
   ConfigV1(const ConfigV1& other) {
     const char* src = reinterpret_cast<const char*>(&other);
@@ -34,12 +39,77 @@ public:
     std::copy(src, src+other._sizeof(), reinterpret_cast<char*>(this));
     return *this;
   }
+  Archon::ConfigV1::ReadoutMode readoutMode() const { return Archon::ConfigV1::ReadoutMode(_readoutMode); }
+  uint16_t sweepCount() const { return _sweepCount; }
+  uint32_t integrationTime() const { return _integrationTime; }
+  uint32_t nonIntegrationTime() const { return _nonIntegrationTime; }
+  uint32_t preSkipPixels() const { return _preSkipPixels; }
+  uint32_t pixels() const { return _pixels; }
+  uint32_t postSkipPixels() const { return _postSkipPixels; }
+  uint32_t overscanPixels() const { return _overscanPixels; }
+  uint16_t preSkipLines() const { return _preSkipLines; }
+  uint16_t lines() const { return _lines; }
+  uint16_t postSkipLines() const { return _postSkipLines; }
+  uint16_t overScanLines() const { return _overScanLines; }
+  uint16_t horizontalBinning() const { return _horizontalBinning; }
+  uint16_t verticalBinning() const { return _verticalBinning; }
+  uint16_t rgh() const { return _rgh; }
+  uint16_t rgl() const { return _rgl; }
+  uint16_t shp() const { return _shp; }
+  uint16_t shd() const { return _shd; }
+  uint16_t st() const { return _st; }
+  uint16_t stm1() const { return _stm1; }
+  uint16_t at() const { return _at; }
+  uint16_t dwell1() const { return _dwell1; }
+  uint16_t dwell2() const { return _dwell2; }
+  int16_t rgHigh() const { return _rgHigh; }
+  int16_t rgLow() const { return _rgLow; }
+  int16_t sHigh() const { return _sHigh; }
+  int16_t sLow() const { return _sLow; }
+  int16_t aHigh() const { return _aHigh; }
+  int16_t aLow() const { return _aLow; }
+  int16_t rgSlew() const { return _rgSlew; }
+  int16_t sSlew() const { return _sSlew; }
+  int16_t aSlew() const { return _aSlew; }
   /** The path to an acf file to use with the camera. */
   const char* config() const { return _config; }
-  static uint32_t _sizeof() { return ((((0+(1*(FILENAME_CHAR_MAX)))+1)-1)/1)*1; }
+  static uint32_t _sizeof() { return ((((76+(1*(FILENAME_CHAR_MAX)))+4)-1)/4)*4; }
 private:
+  uint16_t	_readoutMode;
+  uint16_t	_sweepCount;
+  uint32_t	_integrationTime;
+  uint32_t	_nonIntegrationTime;
+  uint32_t	_preSkipPixels;
+  uint32_t	_pixels;
+  uint32_t	_postSkipPixels;
+  uint32_t	_overscanPixels;
+  uint16_t	_preSkipLines;
+  uint16_t	_lines;
+  uint16_t	_postSkipLines;
+  uint16_t	_overScanLines;
+  uint16_t	_horizontalBinning;
+  uint16_t	_verticalBinning;
+  uint16_t	_rgh;
+  uint16_t	_rgl;
+  uint16_t	_shp;
+  uint16_t	_shd;
+  uint16_t	_st;
+  uint16_t	_stm1;
+  uint16_t	_at;
+  uint16_t	_dwell1;
+  uint16_t	_dwell2;
+  int16_t	_rgHigh;
+  int16_t	_rgLow;
+  int16_t	_sHigh;
+  int16_t	_sLow;
+  int16_t	_aHigh;
+  int16_t	_aLow;
+  int16_t	_rgSlew;
+  int16_t	_sSlew;
+  int16_t	_aSlew;
   char	_config[FILENAME_CHAR_MAX];	/**< The path to an acf file to use with the camera. */
 };
+std::ostream& operator<<(std::ostream& str, Archon::ConfigV1::ReadoutMode enval);
 } // namespace Archon
 } // namespace Pds
 #endif // PDS_ARCHON_DDL_H
--- psddl/src/archon.ddl.cpp
+++ psddl/src/archon.ddl.cpp
@@ -6,9 +6,27 @@
 #include <iostream>
 namespace Pds {
 namespace Archon {
-ConfigV1::ConfigV1(const char* arg__config)
+ConfigV1::ConfigV1(Archon::ConfigV1::ReadoutMode arg__readoutMode, uint16_t arg__sweepCount, uint32_t arg__integrationTime, uint32_t arg__nonIntegrationTime, uint32_t arg__preSkipPixels, uint32_t arg__pixels, uint32_t arg__postSkipPixels, uint32_t arg__overscanPixels, uint16_t arg__preSkipLines, uint16_t arg__lines, uint16_t arg__postSkipLines, uint16_t arg__overScanLines, uint16_t arg__horizontalBinning, uint16_t arg__verticalBinning, uint16_t arg__rgh, uint16_t arg__rgl, uint16_t arg__shp, uint16_t arg__shd, uint16_t arg__st, uint16_t arg__stm1, uint16_t arg__at, uint16_t arg__dwell1, uint16_t arg__dwell2, int16_t arg__rgHigh, int16_t arg__rgLow, int16_t arg__sHigh, int16_t arg__sLow, int16_t arg__aHigh, int16_t arg__aLow, int16_t arg__rgSlew, int16_t arg__sSlew, int16_t arg__aSlew, const char* arg__config)
+    : _readoutMode(arg__readoutMode), _sweepCount(arg__sweepCount), _integrationTime(arg__integrationTime), _nonIntegrationTime(arg__nonIntegrationTime), _preSkipPixels(arg__preSkipPixels), _pixels(arg__pixels), _postSkipPixels(arg__postSkipPixels), _overscanPixels(arg__overscanPixels), _preSkipLines(arg__preSkipLines), _lines(arg__lines), _postSkipLines(arg__postSkipLines), _overScanLines(arg__overScanLines), _horizontalBinning(arg__horizontalBinning), _verticalBinning(arg__verticalBinning), _rgh(arg__rgh), _rgl(arg__rgl), _shp(arg__shp), _shd(arg__shd), _st(arg__st), _stm1(arg__stm1), _at(arg__at), _dwell1(arg__dwell1), _dwell2(arg__dwell2), _rgHigh(arg__rgHigh), _rgLow(arg__rgLow), _sHigh(arg__sHigh), _sLow(arg__sLow), _aHigh(arg__aHigh), _aLow(arg__aLow), _rgSlew(arg__rgSlew), _sSlew(arg__sSlew), _aSlew(arg__aSlew)
 {
   if (arg__config) std::copy(arg__config, arg__config+(256), &_config[0]);
+}
+std::ostream& operator<<(std::ostream& str, Archon::ConfigV1::ReadoutMode enval) {
+  const char* val;
+  switch (enval) {
+  case Archon::ConfigV1::Single:
+    val = "Single";
+    break;
+  case Archon::ConfigV1::Continuous:
+    val = "Continuous";
+    break;
+  case Archon::ConfigV1::Triggered:
+    val = "Triggered";
+    break;
+  default:
+    return str << "ReadoutMode(" << int(enval) << ")";
+  }
+  return str << val;
 }
 } // namespace Archon
 } // namespace Pds
